2cd943a5a5dbac6b9037dc0705119e7e8208b000
Chris Elrod <elrodc@gmail.com>
2021-10-12 14:07:03 -0400

    Support passing thread mask

--- src/request.jl
+++ src/request.jl
@@ -25,17 +25,30 @@ function free_threads!(freed_threads)
   _atomic_or!(worker_pointer(), freed_threads)
   nothing
 end
-@inline function _request_threads(num_requested::UInt32, wp::Ptr, ::StaticInt{N}) where {N}
-  ui, ft, num_requested, wp = __request_threads(num_requested, wp)
-  uit, ftt = _request_threads(num_requested, wp, StaticInt{N}()-StaticInt{1}())
+@inline _remaining(x::Tuple) = Base.tail(x)
+@inline _remaining(@nospecialize(x)) = nothing
+@inline _first(x::Tuple{}) = nothing
+@inline _first(x::Tuple{X,Vararg}) where {X<:Unsigned} = getfield(x,1)
+@inline _first(x::Union{Unsigned,Nothing}) = x
+@inline function _request_threads(num_requested::UInt32, wp::Ptr, ::StaticInt{N}, threadmask) where {N}
+  ui, ft, num_requested, wp = __request_threads(num_requested, wp, _first(threadmask))
+  uit, ftt = _request_threads(num_requested, wp, StaticInt{N}()-StaticInt{1}(), _remaining(threadmask))
   (ui, uit...), (ft, ftt...)
 end
-@inline function _request_threads(num_requested::UInt32, wp::Ptr, ::StaticInt{1})
-  ui, ft, num_requested, wp = __request_threads(num_requested, wp)
+@inline function _request_threads(num_requested::UInt32, wp::Ptr, ::StaticInt{1}, threadmask)
+  ui, ft, num_requested, wp = __request_threads(num_requested, wp, _first(threadmask))
   (ui, ), (ft, )
 end
+@inline function _exchange_mask!(wp, ::Nothing)
+  all_threads = _atomic_xchg!(wp, zero(worker_type()))
+  all_threads, all_threads
+end
+@inline function _exchange_mask!(wp, threadmask::Unsigned)
+  all_threads = _atomic_xchg!(wp, ~(threadmask % worker_type()))
+  all_threads, all_threads & threadmask
+end
 # @inline function __request_threads(num_requested::UInt32, wp::Ptr, reserved_threads)
-@inline function __request_threads(num_requested::UInt32, wp::Ptr)
+@inline function __request_threads(num_requested::UInt32, wp::Ptr, threadmask)
   no_threads = zero(worker_type())
   if num_requested % Int32 ≤ zero(Int32)
     return UnsignedIteratorEarlyStop(zero(worker_type()), 0x00000000), no_threads, 0x00000000, wp
@@ -43,7 +56,8 @@ end
   # to get more, we xchng, setting all to `0`
   # then see which we need, and free those we aren't using.
   wpret = wp + 8 # (worker_type() === UInt64) | (worker_mask_count() === StaticInt(1)) #, so adding 8 is fine.
-  _all_threads = all_threads = _atomic_xchg!(wp, no_threads)
+  # _all_threads = all_threads = _apply_mask(_atomic_xchg!(wp, no_threads), threadmask)
+  _all_threads, all_threads = _exchange_mask!(wp, threadmask)
   additional_threads = count_ones(all_threads) % UInt32
   # num_requested === StaticInt{-1}() && return reserved_threads, all_threads
   if num_requested === StaticInt{-1}()
@@ -67,7 +81,8 @@ end
   _atomic_store!(wp, _all_threads & (~all_threads))
   return UnsignedIteratorEarlyStop(all_threads, num_requested), all_threads, 0x00000000, wpret
 end
-@inline function request_threads(num_requested)
-  _request_threads(num_requested % UInt32, worker_pointer(), worker_mask_count())
+@inline function request_threads(num_requested, threadmask)
+  _request_threads(num_requested % UInt32, worker_pointer(), worker_mask_count(), threadmask)
 end
+@inline request_threads(num_requested) = request_threads(num_requested, nothing)
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -8,6 +8,13 @@ using Test
   @test sum(map(length, threads)) == (PolyesterWeave.num_threads())-1
   map(PolyesterWeave.free_threads!, torelease)
 
+  r1 = PolyesterWeave.request_threads(Sys.CPU_THREADS, 0x0a)
+  @test (r1[2][1] & 0x0a) == r1[2][1]
+  r2 = PolyesterWeave.request_threads(Sys.CPU_THREADS, 0xff)
+  @test (r2[2][1] & 0xff) == r2[2][1]
+  @test count_ones(r1[2][1] ⊻ r2[2][1]) ≤ min(8, Sys.CPU_THREADS)
+  @test iszero(r1[2][1] & r2[2][1])
+  PolyesterWeave.free_threads!(r1[2][1]); PolyesterWeave.free_threads!(r2[2][1])
   
   @testset "Valid State" begin
     @test sum(map(count_ones, PolyesterWeave.WORKERS[])) == min(512, PolyesterWeave.dynamic_thread_count() - 1)
