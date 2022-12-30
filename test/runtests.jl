b476a4ffc6ebbc1da831ec606364cf4364abc3b5
Stefan Krastanov <stefan@krastanov.org>
2022-12-30 00:02:39 -0500

    run tests with more than 64 cpu threads

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ using Test
   threads, torelease = PolyesterWeave.request_threads(Threads.nthreads()-1)
   @test threads isa NTuple{Int(PolyesterWeave.worker_mask_count()),PolyesterWeave.UnsignedIteratorEarlyStop{UInt}}
   @test sum(map(length, threads)) == (PolyesterWeave.num_threads())-1
-  map(PolyesterWeave.free_threads!, torelease)
+  PolyesterWeave.free_threads!(torelease)
 
   r1 = PolyesterWeave.request_threads(Sys.CPU_THREADS, 0x0a)
   @test (r1[2][1] & 0x0a) == r1[2][1]
@@ -14,11 +14,10 @@ using Test
   @test (r2[2][1] & 0xff) == r2[2][1]
   @test count_ones(r1[2][1] ⊻ r2[2][1]) ≤ min(8, Sys.CPU_THREADS)
   @test iszero(r1[2][1] & r2[2][1])
-  PolyesterWeave.free_threads!(r1[2][1]); PolyesterWeave.free_threads!(r2[2][1])
-  
+  PolyesterWeave.free_threads!(r1[2]); PolyesterWeave.free_threads!(r2[2])
+
   @testset "Valid State" begin
     @test sum(map(count_ones, PolyesterWeave.WORKERS[])) == min(512, PolyesterWeave.dynamic_thread_count() - 1)
   end
 end
 Aqua.test_all(PolyesterWeave)
-
